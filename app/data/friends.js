7c7bb3854f23eafd93882fc89810ca3f34e95662
rigel <43711248+rigelselmani@users.noreply.github.com>
2019-03-25 00:03:27 -0400

    add new files

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Stut adsa',
+      profilePic: 'https://unsplash.com/photos/pNMH4SDf8us',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
